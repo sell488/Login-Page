10f09bc94cc5d1e6944e0a6d62c070702997c034
sell488 <sell488sftoday@gmail.com>
2017-11-15 18:11:53 -0600

    Scene changing

--- src/JavaFX4.java
+++ src/JavaFX4.java
@@ -3,12 +3,15 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class JavaFX4 extends Application {
 
-    Button button1;
+    Stage window1;
+    Scene scene1, scene2;
 
     public static void main(String[] args) {
         launch(args);
@@ -17,21 +20,28 @@ public class JavaFX4 extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Test4");
+        window1 = primaryStage;
 
-        button1 = new Button("Test 2.4");
+        Label label1 = new Label("Welcome");
+        Button button1 = new Button("Go to Scene 2");
+        button1.setOnAction((e -> window1.setScene(scene2)));
 
-        button1.setOnAction(e -> {
-            System.out.println("Hi");
-            System.out.println("HELLO");
-        });
+        //First layout
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 400, 200);
 
-        StackPane layout1 = new StackPane();
-        layout1.getChildren().add(button1);
+        StackPane layout2 = new StackPane();
+        Button button2 = new Button("Go to scene 1");
+        button2.setOnAction(e -> window1.setScene(scene1));
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 400, 200);
 
-        Scene scene1 = new Scene(layout1, 300, 250);
-        primaryStage.setScene(scene1);
-        primaryStage.show();
+        window1.setScene(scene1);
+        window1.setTitle("Scene1");
+        window1.show();
+
+
     }
 
 }
